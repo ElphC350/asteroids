c5d637e6abf8782df6d79dc5fe55dbb464b37614
ElphC350 <tk.correa89@gmail.com>
2024-11-03 14:32:35 -0600

    (1) Added pygame groups: updateable, drawable. (2) Assigned to new Player class static field: containers. (3) Updated game loop to use pygame groups instead of individual objects.

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clockObj = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     playerObj = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        playerObj.draw(screen)
-        playerObj.update(dt)
+        for u_item in updateable:
+            u_item.update(dt)
+        for d_item in drawable:
+            d_item.draw(screen)
 
         pygame.display.flip()
         dt = clockObj.tick(60) / 1000.0
