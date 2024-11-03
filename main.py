42bcfa29ad9e93baec2c8c86249abfeec537baa3
ElphC350 <tk.correa89@gmail.com>
2024-11-03 13:37:59 -0600

    Added PLAYER variable and playerObj: render triangle, rotate, and update methods

--- main.py
+++ main.py
@@ -3,17 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clockObj = pygame.time.Clock()
+    dt = 0
+    playerObj = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        playerObj.draw(screen)
+        playerObj.update(dt)
+
         pygame.display.flip()
+        dt = clockObj.tick(60) / 1000.0
+
     print(f"Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
